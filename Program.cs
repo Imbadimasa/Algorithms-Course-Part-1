88a8cb39b5f8ad5d79b4a1c2a6c9a27d3c790a39
Imbadimasa <chalganovdima@gmail.com>
2021-10-03 21:05:26 -0400

    First Task for Algorithms

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace Algorithms_Course_Part_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("Please enter an integer to check");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int d = 0;
+                int i = 2;
+                int result = simpleNumberCheck(n, d, i);
+            }
+        }
+        public static int simpleNumberCheck(int numberToCheck, int confirmationNumber, int dividend)
+        {
+            while (dividend < numberToCheck)
+            {
+                var result = numberToCheck % dividend;
+                if (result == 0)
+                {
+                    confirmationNumber++;
+                    dividend++;
+                }
+                else
+                {
+                    dividend++;
+                }
+            }
+            if (confirmationNumber == 0)
+            {
+                Console.WriteLine("The number is simple");
+            }
+            else
+            {
+                Console.WriteLine("The number is not simple");
+            }
+            return 0;
         }
     }
 }
