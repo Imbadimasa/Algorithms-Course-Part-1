6960cf6588b827c5a5731a27b34a54b73cee92d0
Imbadimasa <chalganovdima@gmail.com>
2021-10-03 20:29:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms_Course_Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
