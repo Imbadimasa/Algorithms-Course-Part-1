5b123496c7239a4e20a1df35f9f370b1a1d63a64
Imbadimasa <chalganovdima@gmail.com>
2021-10-03 21:20:20 -0400

    Attempt to use classes instead of Main

---
+++ Homework_1_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Course_Part_1
+{
+    class Homework11
+    {
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter an integer to check");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int d = 0;
+                int i = 2;
+                int result = simpleNumberCheck(n, d, i);
+            }
+        }
+        
+    static int simpleNumberCheck(int numberToCheck, int confirmationNumber, int dividend)
+        {
+            while (dividend < numberToCheck)
+            {
+                var result = numberToCheck % dividend;
+                if (result == 0)
+                {
+                    confirmationNumber++;
+                    dividend++;
+                }
+                else
+                {
+                    dividend++;
+                }
+            }
+            if (confirmationNumber == 0)
+            {
+                Console.WriteLine("The number is simple");
+            }
+            else
+            {
+                Console.WriteLine("The number is not simple");
+            }
+            return 0;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -6,39 +6,8 @@ namespace Algorithms_Course_Part_1
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine("Please enter an integer to check");
-                int n = Convert.ToInt32(Console.ReadLine());
-                int d = 0;
-                int i = 2;
-                int result = simpleNumberCheck(n, d, i);
-            }
-        }
-        public static int simpleNumberCheck(int numberToCheck, int confirmationNumber, int dividend)
-        {
-            while (dividend < numberToCheck)
-            {
-                var result = numberToCheck % dividend;
-                if (result == 0)
-                {
-                    confirmationNumber++;
-                    dividend++;
-                }
-                else
-                {
-                    dividend++;
-                }
-            }
-            if (confirmationNumber == 0)
-            {
-                Console.WriteLine("The number is simple");
-            }
-            else
-            {
-                Console.WriteLine("The number is not simple");
-            }
-            return 0;
+            Homework11 Homework_11 = new Homework11();
+            Homework_11.Run();
         }
     }
 }
